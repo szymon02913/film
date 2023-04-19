0dce84e37d725a3cb9c4f92104870989b229d09a
Szymon <113582467+szymon02913@users.noreply.github.com>
2023-04-19 10:11:19 +0200

    Add files via upload

---
+++ videoo.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Video on Demand</title>
+<link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+<div id="banerlewy">
+    <h1> Internetowa wypozyczalnia filmow</h1>
+    </div>
+<div id="banerprawy">
+<table>
+<tr><td>Kryminał</td><td>Horror</td><td>Przygodowy</td> </tr>
+<tr> <td>20</td><td>30</td><td>20</td></tr>
+</table>
+</div>
+<div id="polecamy">
+<h3>polecamy</h3>
+</div>
+<div id="filmyfantastyczne">
+<h3>filmy fantastyczne</h3>
+
+
+</div>
+
+
+</div>
+
+<div id="stopka">
+<p>Strone wykonał</p>
+    </div>
+
+
+</body>
+</html>
